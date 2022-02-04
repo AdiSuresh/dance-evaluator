71ba3689ed30952b7a06b4e28c216e2a45fda2fd
Adithyan S Raj <adithyansraj20@gmail.com>
2022-02-04 16:04:08 +0530

    Sample_output2 csv file added

--- Dancing AI Ultimate/Assets/Scripts/Dance.cs
+++ Dancing AI Ultimate/Assets/Scripts/Dance.cs
@@ -4,21 +4,18 @@ using UnityEngine;
 
 public class Dance : MonoBehaviour
 {
-    int size = 
-    public GameObject Armature;
-    public GameObject[] Array;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Array[0] = Armature.transform.GetChild(0).gameObject;
+        
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        //LoweArmL.transform.position += new Vector3(0.0005f, 0, 0);
-        Array[0].transform.position += new Vector3(0.0005f, 0, 0);
+
     }
 }
