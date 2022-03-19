adcd25e80a0c56674cec78684b43c9e1a7512515
Adithyan S Raj <adithyansraj20@gmail.com>
2022-03-19 16:57:53 +0530

    Update Home_Screen.py

--- fyp-gui/Home_Screen.py
+++ fyp-gui/Home_Screen.py
@@ -1,6 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
 
 
 class App(QWidget):
@@ -14,13 +13,39 @@ class App(QWidget):
         self.height = 720
         self.initUI()
 
+    def button_clicked(self):
+        self.label.setText("you pressed the button")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.label = QLabel(self)
+        self.label.setText("my first label!            ")
+        self.label.move(50, 50)
+
+        self.b1 = QPushButton(self)
+        self.b1.setText("Upload Video File to Estimate")
+        self.b1.move(200,200)
+        self.b1.clicked.connect(self.button_clicked)
+
+        self.b2 = QPushButton(self)
+        self.b2.setText("Record Your Dance Moves in Score")
+        self.b2.move(800, 200)
+
+        self.b3 = QPushButton(self)
+        self.b3.setText("Comparing Dance Moves")
+        self.b3.move(500, 500)
+
         self.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
+    ex.show()
     sys.exit(app.exec_())
