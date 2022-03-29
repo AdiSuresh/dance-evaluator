96c4e93972f80f2dc1a015feae1ee1d2fad7133a
Adithyan S Raj <adithyansraj20@gmail.com>
2022-03-29 16:37:52 +0530

    Update Main.py

--- fyp-gui/Main.py
+++ fyp-gui/Main.py
@@ -1,12 +1,4 @@
-import sys
-# import shutil
-from PyQt5.QtWidgets import *
-import Copy_File
-import Home_Screen
+import os
 
-
-
-if __name__=='__main__':    
-    app = QApplication([])
-    execfile('Home_Screen')
-    
+if __name__=='__main__': 
+    os.system('python Home_Screen.py')
